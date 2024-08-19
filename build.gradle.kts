d56c4f05622f2a65f45a4ac2a84f71b15adabbd6
Simon Scholz <Simon.Sleeeep@outlook.com>
2024-08-19 11:34:24 +0800

    chore: Add Kotlin stdlib

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,19 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
