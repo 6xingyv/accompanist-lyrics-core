f0788c36e51d273c0481851af4217f359a4dfc21
Simon Scholz <Simon.Sleeeep@outlook.com>
2024-08-19 11:34:24 +0800

    feat: TTML space detect
    feat: TTML singer alignment

--- src/main/kotlin/io/mocha/music/lyrics/utils/StringUtils.kt
+++ src/main/kotlin/io/mocha/music/lyrics/utils/StringUtils.kt
@@ -5,30 +5,33 @@ fun String.isDigitsOnly(): Boolean {
 }
 
 fun String.parseAsTime(): Int {
-
-    val parts = this.split(":")
-    val time= when (parts.size) {
-        3 -> {
-            val hours = parts[0].toInt() * 3600 * 1000
-            val minutes = parts[1].toInt() * 60 * 1000
-            val secondsAndMillis = parts[2].split(".")
-            val seconds = secondsAndMillis[0].toInt() * 1000
-            val millis =
-                if (secondsAndMillis.size > 1) secondsAndMillis[1].toInt()  else 0
-            println(parts[0]+ ":"+parts[1]+":"+parts[2])
-            hours + minutes + seconds + millis
+    return try {
+        val parts = this.split(":")
+        when (parts.size) {
+            3 -> {
+                val hours = parts[0].toIntOrNull()?.let { it * 3600 * 1000 } ?: 0
+                val minutes = parts[1].toIntOrNull()?.let { it * 60 * 1000 } ?: 0
+                val (secondsStr, millisStr) = parts[2].split(".")
+                val seconds = secondsStr.toIntOrNull()?.let { it * 1000 } ?: 0
+                val millis = millisStr.toIntOrNull() ?: 0
+                hours + minutes + seconds + millis
+            }
+            2 -> {
+                val minutes = parts[0].toIntOrNull()?.let { it * 60 * 1000 } ?: 0
+                val (secondsStr, millisStr) = parts[1].split(".")
+                val seconds = secondsStr.toIntOrNull()?.let { it * 1000 } ?: 0
+                val millis = millisStr.toIntOrNull() ?: 0
+                minutes + seconds + millis
+            }
+            1 -> {
+                val (secondsStr, millisStr) = parts[0].split(".")
+                val seconds = secondsStr.toIntOrNull()?.let { it * 1000 } ?: 0
+                val millis = millisStr.toIntOrNull() ?: 0
+                seconds + millis
+            }
+            else -> 0
         }
-
-        2 -> {
-            val minutes = parts[0].toInt() * 60 * 1000
-            val secondsAndMillis = parts[1].split(".")
-            val seconds = secondsAndMillis[0].toInt() * 1000
-            val millis =
-                if (secondsAndMillis.size > 1) secondsAndMillis[1].toInt()  else 0
-            minutes + seconds + millis
-        }
-
-        else -> 0
+    } catch (_: Exception) {
+        0
     }
-    return time
 }
